3c197a57d43930e056b0db84337e3c3bc451c463
Elise Navarro <elise.marie.navarro@gmail.com>
2020-02-27 11:49:25 -0700

    Correction to Routes Resources method

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'projects/show'
   root to: 'pages#home'
 
-  resouces :projects, only: [:show]
+  resources :projects, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
