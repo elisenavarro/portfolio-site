6cc4d97b9447a21482307baa1ce093d93ac2062d
Elise Navarro <elise.marie.navarro@gmail.com>
2020-03-16 12:11:38 -0600

    Meta Tag Setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def default_url_options
-    { host: ENV["DOMAIN"] || "localhost:3000" }
+    { host: ENV["ELISENAVARRO"] || "localhost:3000" }
   end
 end
