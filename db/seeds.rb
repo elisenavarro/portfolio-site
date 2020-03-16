d9c460ea32c4ecc75474052542486cf821a22b28
Elise Navarro <elise.marie.navarro@gmail.com>
2020-03-16 12:40:50 -0600

    Seed Updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,5 @@ hivie = Project.create(title: 'HiVie', description: 'A mobile-first web applicat
 hivie_img1 = Project.create(project: hivie, image_url: 'HiVie_1.png')
 hivie_img2 = Project.create(project: hivie, image_url: 'HiVie_2.png')
 hivie_img3 = Project.create(project: hivie, image_url: 'HiVie_3.png')
+
+rent_tent = Project.create(title: "Rent-a-Tent", description: 'An Airbnb clone web application, built in a 5-day sprint. Allows users to rent tents for camping trips.', image_gallery: 'rent-a-tent.jpg', tagline: 'Camp more. Worry Less', site_url: 'https://rent-a-tent-341.herokuapp.com/', source_url: 'https://github.com/Hamzif/-rent_a_tent')
